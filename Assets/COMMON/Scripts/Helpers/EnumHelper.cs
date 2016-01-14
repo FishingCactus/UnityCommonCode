15e0b45c31e5315599213691749950c817e41f18
Michael Delva <michael.delva@fishingcactus.com>
2016-01-14 14:26:18 +0100

    Added EnumHelper

---
+++ Assets/COMMON/Scripts/Helpers/EnumHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FishingCactus
+{
+    public class EnumHelper
+    {
+        public static T ParseEnum<T>( string value )
+        {
+            return ( T )Enum.Parse( typeof( T ), value, true );
+        }
+    }
+}
