256ead15973ebcc3b584eae51a2aaeb0ea28fbca
Michael Delva <michael.delva@fishingcactus.com>
2016-01-21 11:22:00 +0100

    StringHelper

---
+++ Assets/COMMON/Scripts/Helpers/StringHelper.cs
@@ -0,0 +1,13 @@
+﻿public static class StringHelper
+{
+    public static string Capitalize( string str )
+    {
+        if ( str == null )
+            return null;
+
+        if ( str.Length > 1 )
+            return char.ToUpper( str[ 0 ] ) + str.Substring( 1 );
+
+        return str.ToUpper();
+    }
+}
