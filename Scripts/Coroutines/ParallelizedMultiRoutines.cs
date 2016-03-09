36747ee329bbbf0e20de8c0e67ee856f82b68fa3
Michael Delva <michael.delva@fishingcactus.com>
2016-03-09 21:57:42 +0100

    Added ParallelizedMultiRoutines

---
+++ Scripts/Coroutines/ParallelizedMultiRoutines.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace FishingCactus
+{
+    public class ParallelizedMultiRoutines : Routine
+    {
+        private readonly List<IEnumerable<Routine>> routineList;
+        private int executionCounter;
+
+        public ParallelizedMultiRoutines( List<IEnumerable<Routine>> routine_list )
+        {
+            routineList = routine_list;
+            executionCounter = routineList.Count;
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            foreach( var routine_enumerable in routineList )
+            {
+                CoroutineHelper.Get.BeginExecute( routine_enumerable, RoutineCompleted );
+            }
+        }
+
+        private void RoutineCompleted( object sender, ResultCompletionEventArgs args )
+        {
+            executionCounter--;
+
+            if ( executionCounter == 0 )
+            {
+                CallOnCompleted( true );
+            }
+        }
+    }
+}
