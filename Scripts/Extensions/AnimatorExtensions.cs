c4a46f024a03b2004bbb64ae39ca8a77bd3c2c6c
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-03-27 14:16:09 +0200

    Animator 'HasParameter()' added.

---
+++ Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace FishingCactus
+{
+    public static class AnimatorExtensions
+    {
+        // -- PUBLIC
+
+        public static bool HasParameter(
+            this Animator animator,
+            string parameter_name
+            )
+        {
+            foreach ( AnimatorControllerParameter param in animator.parameters )
+            {
+                if ( param.name == parameter_name )
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static bool HasParameter(
+            this Animator animator,
+            int  parameter_hash_id
+            )
+        {
+            foreach ( AnimatorControllerParameter param in animator.parameters )
+            {
+                if ( param.nameHash == parameter_hash_id )
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
