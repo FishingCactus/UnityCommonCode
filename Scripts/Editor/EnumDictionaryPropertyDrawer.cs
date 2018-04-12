5b6bd9b93b1f52ed9d1129abc660b4b565e9b05e
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-04-12 18:08:28 +0200

    EnumDictionary class & editor added.

---
+++ Scripts/Editor/EnumDictionaryPropertyDrawer.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EnumDictionaryPropertyDrawer : PropertyDrawer
+{
+    // -- UNITY
+
+    public override void OnGUI(
+        Rect position,
+        SerializedProperty property,
+        GUIContent label
+        )
+    {
+        Rect gui_rectangle;
+        SerializedProperty type_property = property.FindPropertyRelative("EnumNameTable");
+        SerializedProperty values_property = property.FindPropertyRelative("ValueTable");
+
+        Debug.Assert( type_property.arraySize == values_property.arraySize, "EnumDictionary array sizes mismatched." );
+
+        gui_rectangle = position;
+
+        EditorGUI.BeginProperty( position, label, property );
+
+        gui_rectangle = position;
+        gui_rectangle.y += 10.0f;
+        gui_rectangle.height = 18.0f;
+
+        property.isExpanded = EditorGUI.Foldout( gui_rectangle, property.isExpanded, label, InternalEditorStyle.Foldout );
+
+        if( property.isExpanded )
+        {
+            gui_rectangle = EditorGUI.IndentedRect(gui_rectangle);
+
+            float element_x = gui_rectangle.x;
+            float element_width = gui_rectangle.width;
+
+            gui_rectangle.width = element_width * 0.5f;
+
+            for( int name_index = 0; name_index < type_property.arraySize; name_index++ )
+            {
+                gui_rectangle.y += 18.0f;
+
+                gui_rectangle.x = element_x;
+                EditorGUI.LabelField(gui_rectangle, new GUIContent(type_property.GetArrayElementAtIndex(name_index).stringValue));
+
+                gui_rectangle.x += gui_rectangle.width;
+                EditorGUI.PropertyField(gui_rectangle, values_property.GetArrayElementAtIndex(name_index), GUIContent.none );
+            }
+
+            gui_rectangle.x = element_x;
+            gui_rectangle.width = element_width;
+        }
+
+        EditorGUI.EndProperty();
+
+        property.serializedObject.ApplyModifiedProperties();
+    }
+
+    public override float GetPropertyHeight(
+        SerializedProperty property,
+        GUIContent label
+        )
+    {
+        float height_to_use = 28.0f;
+
+        if (property.isExpanded)
+        {
+            SerializedProperty list_property = property.FindPropertyRelative("EnumNameTable");
+
+            height_to_use += 20.0f * list_property.arraySize;
+        }
+
+        return height_to_use;
+    }
+}
---
+++ Scripts/Helpers/EnumDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class EnumDictionary<TEnum, TObject> where TEnum : struct,
+    System.IConvertible
+{
+    // -- PUBLIC
+
+    public TObject[] ValueTable;
+    public List<string> EnumNameTable;
+
+    public EnumDictionary()
+    {
+        ResetTables();
+    }
+
+    public TObject this[string element_name]
+    {
+        get { return this[EnumNameTable.IndexOf(element_name)]; }
+        set { this[EnumNameTable.IndexOf(element_name)] = value; }
+    }
+
+    public TObject this[ int element_index ]
+    {
+        get { return ValueTable[ element_index ]; }
+        set { ValueTable[element_index] = value; }
+    }
+
+#if UNITY_EDITOR
+    public void CheckEnumSizeChange()
+    {
+        TObject[] backup_value_array = ValueTable;
+        List<string> backup_enum_name_table = EnumNameTable;
+
+        ResetTables();
+
+        foreach( string enum_name in EnumNameTable )
+        {
+            if( backup_enum_name_table.Contains( enum_name ) )
+            {
+                ValueTable[EnumNameTable.IndexOf( enum_name )] = backup_value_array[backup_enum_name_table.IndexOf( enum_name )];
+            }
+        }
+    }
+#endif
+
+    // -- PRIVATE
+
+    private void ResetTables()
+    {
+        EnumNameTable = new List<string>();
+        EnumNameTable.AddRange( System.Enum.GetNames( typeof( TEnum ) ) );
+
+        ValueTable = new TObject[EnumNameTable.Count];
+    }
+}
+
