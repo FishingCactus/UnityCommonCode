c70a4f04752b86a4d54b17d726583e07bee7a1e3
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-04-13 14:38:32 +0200

    review + cs.

--- Scripts/Helpers/EnumDictionary.cs
+++ Scripts/Helpers/EnumDictionary.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
 [System.Serializable]
 public class EnumDictionary<TEnum, TObject> where TEnum : struct,
     System.IConvertible
 {
     // -- PUBLIC
-
-    public TObject[] ValueTable;
-    public List<string> EnumNameTable;
 
     public EnumDictionary()
     {
@@ -16,29 +14,29 @@ public class EnumDictionary<TEnum, TObject> where TEnum : struct,
 
     public TObject this[string element_name]
     {
-        get { return this[EnumNameTable.IndexOf(element_name)]; }
-        set { this[EnumNameTable.IndexOf(element_name)] = value; }
+        get { return this[_EnumNameTable.IndexOf(element_name)]; }
+        set { this[_EnumNameTable.IndexOf(element_name)] = value; }
     }
 
     public TObject this[ int element_index ]
     {
-        get { return ValueTable[ element_index ]; }
-        set { ValueTable[element_index] = value; }
+        get { return _ValueTable[ element_index ]; }
+        set { _ValueTable[element_index] = value; }
     }
 
 #if UNITY_EDITOR
     public void CheckEnumSizeChange()
     {
-        TObject[] backup_value_array = ValueTable;
-        List<string> backup_enum_name_table = EnumNameTable;
+        TObject[] backup_value_array = _ValueTable;
+        List<string> backup_enum_name_table = _EnumNameTable;
 
         ResetTables();
 
-        foreach( string enum_name in EnumNameTable )
+        foreach( string enum_name in _EnumNameTable )
         {
             if( backup_enum_name_table.Contains( enum_name ) )
             {
-                ValueTable[EnumNameTable.IndexOf( enum_name )] = backup_value_array[backup_enum_name_table.IndexOf( enum_name )];
+                _ValueTable[_EnumNameTable.IndexOf( enum_name )] = backup_value_array[backup_enum_name_table.IndexOf( enum_name )];
             }
         }
     }
@@ -46,15 +44,20 @@ public class EnumDictionary<TEnum, TObject> where TEnum : struct,
 
     // -- PRIVATE
 
+    [SerializeField]
+    private TObject[] _ValueTable;
+    [SerializeField]
+    public List<string> _EnumNameTable;
+
     private void ResetTables()
     {
-        EnumNameTable = new List<string>();
-        EnumNameTable.AddRange( System.Enum.GetNames( typeof( TEnum ) ) );
+        _EnumNameTable = new List<string>();
+        _EnumNameTable.AddRange( System.Enum.GetNames( typeof( TEnum ) ) );
 
-        EnumNameTable.Remove( "Count" );
-        EnumNameTable.Remove( "None" );
+        _EnumNameTable.Remove( "Count" );
+        _EnumNameTable.Remove( "None" );
 
-        ValueTable = new TObject[EnumNameTable.Count];
+        _ValueTable = new TObject[_EnumNameTable.Count];
     }
 }
 
