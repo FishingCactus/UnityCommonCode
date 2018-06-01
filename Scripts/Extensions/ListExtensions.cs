7881101fa932ae6f11da0b493ffc411c6b4fa3e7
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-06-01 13:47:33 +0200

    Shuffle() added.

---
+++ Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace FishingCactus
+{
+    public static class ListExtensions
+    {
+        // -- PUBLIC
+
+        public static void Shuffle<T>(
+            this List<T> list
+            )
+        {
+            list.Sort( ( a, b ) => Random.Range( 0, 2 ) == 0 ? 1 : -1 );
+        }
+    }
+}
