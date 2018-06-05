03e8919300beb3bb2cf4de3f8a313b6dfe881a5d
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-06-05 17:10:23 +0200

    Shuffle updated.

--- Scripts/Extensions/ListExtensions.cs
+++ Scripts/Extensions/ListExtensions.cs
@@ -11,7 +11,21 @@ namespace FishingCactus
             this List<T> list
             )
         {
-            list.Sort( ( a, b ) => Random.Range( 0, 2 ) == 0 ? 1 : -1 );
+            //  for i from 0 to n−2 do
+            //      j ← random integer such that i ≤ j < n
+            //      exchange a[i] and a[j]
+
+            int index_to_swap;
+            T backup_value;
+
+            for( int index = 0; index < list.Count - 1; index++ )
+            {
+                index_to_swap = Random.Range( index, list.Count );
+
+                backup_value = list[index];
+                list[index] = list[index_to_swap];
+                list[index_to_swap] = backup_value;
+            }
         }
     }
 }
