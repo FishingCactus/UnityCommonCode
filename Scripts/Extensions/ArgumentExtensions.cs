8ddeacb9da648cc35d8cc7948c5b97289540935c
DESKTOP-M4OUQIC\Fany Bottemanne <fany.bottemanne@gmail.com>
2018-06-11 11:32:20 +0200

    add argumentExtension

---
+++ Scripts/Extensions/ArgumentExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FishingCactus 
+{
+    public class ArgumentExtensions
+    {
+        // -- PUBLIC
+
+        public static bool FindArg(
+            out string value,
+            string name
+        ) {
+            value = null;
+            string[] args = Environment.GetCommandLineArgs();
+            int index = Array.IndexOf(args, name);
+            if (index > -1 && args.Length > index + 1) {
+                value = args[index + 1];
+            }
+
+            return index > -1;
+        }
+
+        public static string GetArg(
+            string name
+        ) {
+            string[] args = Environment.GetCommandLineArgs();
+            int index = Array.IndexOf(args, name);
+
+            if (index > -1 && args.Length > index + 1) {
+                return args[index + 1];
+            }
+            return null;
+        }
+
+        public static bool HasArg(
+            string name
+        ) {
+            string[] args = Environment.GetCommandLineArgs();
+            int index = Array.IndexOf(args, name);
+
+            return index > -1;
+        }
+    }
+}
