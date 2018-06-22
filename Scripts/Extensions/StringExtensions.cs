a013bd6c7f3a4e920ce24b484b9c7dba0aa3a5ac
Thibaut Hanson <thibaut.hanson@fishingcactus.com>
2018-06-22 10:28:57 +0200

    fixed mixed tabs/spaces

--- Scripts/Extensions/StringExtensions.cs
+++ Scripts/Extensions/StringExtensions.cs
@@ -2,28 +2,28 @@ using System;
 
 public static class StringExtensions
 {
-	public static int CountOccurence( this string the_string, string find_string )
-	{
-		int count = 0;
+    public static int CountOccurence( this string the_string, string find_string )
+    {
+        int count = 0;
         int find_index;
-        
+
         for ( int index = 0; index < the_string.Length; index++ )
-		{
-			for( find_index = 0; find_index < find_string.Length; find_index++ )
-			{
-				if ( the_string[ index + find_index ] != find_string[ find_index ] )
-				{
-					break;
-				}
-			}
+        {
+            for ( find_index = 0; find_index < find_string.Length; find_index++ )
+            {
+                if ( the_string[ index + find_index ] != find_string[ find_index ] )
+                {
+                    break;
+                }
+            }
 
-			if ( find_index == find_string.Length )
-			{
-				count++;
-			}
-		}
+            if ( find_index == find_string.Length )
+            {
+                count++;
+            }
+        }
 
-		return count;
-	}
+        return count;
+    }
 }
 
