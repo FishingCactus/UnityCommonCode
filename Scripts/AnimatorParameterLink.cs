8ce083115612d6db74f618a761b259c326a72310
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-07-24 18:04:25 +0200

    Classes added.

---
+++ Scripts/AnimatorParameterLink.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor.Animations;
+
+namespace FishingCactus
+{
+    [System.Serializable]
+    public abstract class AnimatorParameterLink
+    {
+        // -- PUBLIC
+
+#if UNITY_EDITOR
+        public AnimatorController LinkedAnimatorController = null;
+        public string TypeName;
+#endif
+
+        public string ParameterName { get; }
+
+        // -- PRIVATE
+
+        [SerializeField]
+        private string _ParameterName;
+    }
+
+    [System.Serializable]
+    public class BooleanAnimatorParameterLink : AnimatorParameterLink
+    {
+    }
+
+    [System.Serializable]
+    public class IntegerAnimatorParameterLink : AnimatorParameterLink
+    {
+    }
+
+    [System.Serializable]
+    public class FloatAnimatorParameterLink : AnimatorParameterLink
+    {
+    }
+
+    [System.Serializable]
+    public class TriggerAnimatorParameterLink : AnimatorParameterLink
+    {
+    }
+}
