7c9e98a77a5a12c57fa2826f6c114877f93ec5b6
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-08-08 17:22:09 +0200

    TagLink + drawer added.

---
+++ Scripts/TagLink.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FishingCactus
+{
+    [System.Serializable]
+    public class TagLink
+    {
+        // -- PUBLIC
+
+        public string TagName { get { return _TagName; } }
+
+        // -- PRIVATE
+
+        [SerializeField]
+        private string _TagName;
+    }
+}
