5cf97a3afce4a3adf5ccc8b3088b34d66f3bf725
Maxime Rollet <maxime.rollet@fishingcactus.com>
2018-10-25 14:07:35 +0200

    IntegerRange & FloatRange classes added + custom editor.

---
+++ Scripts/Editor/PropertyDrawer/RangeDrawer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace FishingCactus
+{
+    [CustomPropertyDrawer(typeof(IntegerRange), true)]
+    [CustomPropertyDrawer(typeof(FloatRange), true)]
+    public class RangeDrawer : PropertyDrawer
+    {
+        // -- UNITY
+
+        public override void OnGUI(
+            Rect position, 
+            SerializedProperty property, 
+            GUIContent label
+            )
+        {
+            Rect local_rectangle = position;
+            SerializedProperty minimum_value_property = property.FindPropertyRelative( "_MinimumValue" );
+            SerializedProperty maximum_value_property = property.FindPropertyRelative( "_MaximumValue" );
+
+            local_rectangle.width *= 0.4f;
+            EditorGUI.LabelField( local_rectangle, $"{label.text}" );
+
+            local_rectangle.x += local_rectangle.width;
+            local_rectangle.width = position.width * 0.05f;
+            EditorGUI.LabelField( local_rectangle, "[" );
+
+            local_rectangle.x += local_rectangle.width;
+            local_rectangle.width = position.width * 0.20f;
+            EditorGUI.PropertyField( local_rectangle, minimum_value_property, new GUIContent( "" ) );
+
+            local_rectangle.x += local_rectangle.width;
+            local_rectangle.width = position.width * 0.05f;
+            EditorGUI.LabelField( local_rectangle, " ;" );
+
+            local_rectangle.x += local_rectangle.width;
+            local_rectangle.width = position.width * 0.20f;
+            EditorGUI.PropertyField( local_rectangle, maximum_value_property, new GUIContent( "" ) );
+
+            local_rectangle.x += local_rectangle.width;
+            local_rectangle.width = position.width * 0.05f;
+            EditorGUI.LabelField( local_rectangle, maximum_value_property.propertyType == SerializedPropertyType.Integer ? " [" : " ]" );
+        }
+    }
+}
---
+++ Scripts/Math/Range.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace FishingCactus
+{
+    [System.Serializable]
+    public abstract class Range< T >
+    {
+        // -- PUBLIC
+
+        public T MinimumValue { get; private set; }
+        public T MaximumValue { get; private set; }
+
+        public abstract T GetRandomValue();
+
+        // -- PROTECTED
+
+        [SerializeField]
+        protected T _MinimumValue;
+        [SerializeField]
+        protected T _MaximumValue;
+    }
+
+    [System.Serializable]
+    public class IntegerRange : Range< int >
+    {
+        // -- PUBLIC
+
+        public override int GetRandomValue()
+        {
+            return Random.Range( MinimumValue, MaximumValue );
+        }
+    }
+
+    [System.Serializable]
+    public class FloatRange : Range<float>
+    {
+        // -- PUBLIC
+
+        public override float GetRandomValue()
+        {
+            return Random.Range( MinimumValue, MaximumValue );
+        }
+    }
+}
